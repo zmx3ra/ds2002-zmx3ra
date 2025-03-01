e92a1b9c8929cf5fbecb38e4c676899170f80e20
root <root@DESKTOP-PAR04HV>
2025-02-28 23:10:39 -0500

    Lab 4 Push with Comments

--- labs/lab4/script1.sh
+++ labs/lab4/script1.sh
@@ -3,11 +3,12 @@
 argument1=$1
 argument2=$2
 argument3=$3
-
+# adding the positional arguments
 
 aws s3 cp $argument1 s3://$argument2
-
+# this will move the file into the designated bucket
 
 
 aws s3 presign --expires-in $argument3 s3://$argument2/$argument1
 
+# this will make the link 
--- labs/lab4/script2.py
+++ labs/lab4/script2.py
@@ -4,7 +4,10 @@ import requests
 import os
 import boto3
 
+# make the necessary imports
+
 s3=boto3.client('s3', region_name="us-east-1")
+# make the client
 
 def download_file(url, file_path):
     try:
@@ -18,11 +21,14 @@ def download_file(url, file_path):
     
     except requests.exceptions.RequestException as e:
         print(f"Error downloading: {e}")
+# function used to download the file, and ends with it printing the file downloaded to the designated end, also adds in an except if there is an error
 
 # Example usage:
 image_url = "https://www.icegif.com/wp-content/uploads/2022/10/icegif-760.gif"
 file = "downloaded_image.gif"
 path = os.path.join(os.getcwd(), file) # Saves to current directory
+
+# defines the necessary variables
 
 download_file(image_url, path)
 
@@ -43,7 +49,7 @@ from botocore.exceptions import ClientError
 bucket_name='ds2002-zmx3ra'
 object_name='downloaded_image.gif'
 expires_in=3600
-
+# names more variables
 
 def create_presigned_url(bucket_name, object_name, expiration=3600):
     """Generate a presigned URL to share an S3 object
@@ -60,7 +66,7 @@ def create_presigned_url(bucket_name, object_name, expiration=3600):
         response = s3_client.generate_presigned_url('get_object',
                                                     Params={'Bucket': bucket_name,
                                                             'Key': object_name},
-                                                    ExpiresIn=expires_in)
+                                                    ExpiresIn=expires_in) #generates the s3 client
     except ClientError as e:
         logging.error(e)
         return None
@@ -70,3 +76,4 @@ def create_presigned_url(bucket_name, object_name, expiration=3600):
 
 
 # source used: https://stackoverflow.com/questions/39272744/when-to-use-a-boto3-client-and-when-to-use-a-boto3-resource
+# I used all of the sources given in the homework this week!
